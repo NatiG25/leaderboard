c6652a0b69b9b8c9b7deb0f801f20920a7abb25f
NatiG <nati.g5684@gmail.com>
2022-03-17 22:22:51 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const score = document.querySelector('.score');
 const submitBtn = document.querySelector('.submitBtn');
 const refreshBtn = document.querySelector('.refreshBtn');
 const scoreList = document.querySelector('.scoreList');
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/aJpln9nj3naXwqc1haiG/scores/'
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/aJpln9nj3naXwqc1haiG/scores/';
 
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    fetch(url , {
+  e.preventDefault();
+  fetch(url, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
@@ -18,32 +18,33 @@ submitBtn.addEventListener('click', (e) => {
       user: name.value,
       score: score.value,
     }),
-  
+
   })
     .then((response) => response.json())
-    .then((json) => json).catch(error => error);
-    name.value = '';
-    score.value = '';
-})
+    .then((json) => json).catch((error) => error);
+  name.value = '';
+  score.value = '';
+});
 
 const getAPI = async () => {
-    const res = await fetch(url);
-    return await res.json();
-}
+  const res = await fetch(url);
+  const resJson = await res.json();
+  return resJson;
+};
 
 const displayScore = (array) => {
-    array.forEach(item => {
-            scoreList.innerHTML += `<li>
+  array.forEach((item) => {
+    scoreList.innerHTML += `<li>
             <ul>
             <li>${item.user}</li>
             <li>${item.score}</li>
             </ul>
-            </li>`
-        })
-    };
+            </li>`;
+  });
+};
 
-refreshBtn.addEventListener('click',async () => {
-    const data = await getAPI();
-    scoreList.innerHTML = '';
-    displayScore(data.result);
-})
+refreshBtn.addEventListener('click', async () => {
+  const data = await getAPI();
+  scoreList.innerHTML = '';
+  displayScore(data.result);
+});
